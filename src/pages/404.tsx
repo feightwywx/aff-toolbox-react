9a0db83d5f8c7c225be08654f8ad7ed8a24d18ad
feightwywx <kururinmiracle@outlook.com>
2022-09-04 22:57:36 +0800

    Fix: 404页面无法读取i18n信息

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -6,16 +6,31 @@ import {
 } from "@mui/material"
 
 import { Link as GatsbyLink } from "gatsby-theme-material-ui"
+import { graphql } from "gatsby"
 
 // markup
 const NotFoundPage = () => {
   return (
     <Box>
       <Typography variant="h1">404</Typography>
-      <Typography variant='h4'>这里好像什么也没有...</Typography>
+      <Typography variant='h4'>你似乎来到了空无一人的世界...</Typography>
       <Typography variant='h6'><GatsbyLink to='/'>回到主页</GatsbyLink></Typography>
     </Box>
   )
 }
 
 export default NotFoundPage
+
+export const pageQuery = graphql`
+query NotFoundPageQuery($language: String!) {
+    locales: allLocale(filter: {ns: {in: ["common"]}, language: {eq: $language}}) {
+      edges {
+        node {
+          ns
+          data
+          language
+        }
+      }
+    } 
+  }
+`
